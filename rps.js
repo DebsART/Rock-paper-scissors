5b9cb7a261f20ec2b53d40b51247cece2699cd5c
Debbie <debby.ek@yahoo.com>
2022-11-10 16:44:37 -0600

    quick randomized game round btw computer and user

---
+++ rps.js
@@ -0,0 +1,23 @@
+
+function getComputerChoice() {
+let choice = ['rock', 'paper', 'scissors'];
+return choice[Math.floor(Math.random() * choice.length)];
+}
+
+let playerSelection = prompt('make your choice?');
+playerSelection.toUpperCase === playerSelection.toLowerCase;
+let computerSelection = getComputerChoice();
+function game1(playerSelection, computerSelection) {
+    if ((playerSelection == 'rock') && (computerSelection == 'paper')){
+        return 'You lose! paper beats rock!'
+    } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
+        return 'You lose! rock beats scissors!'
+    } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
+       return 'You lose! scissors beats paper!'
+    } else if (playerSelection == computerSelection){
+        return 'A tie! Try again!'
+    } else{
+        return 'You win! great choice!'
+    }
+    console.log(game1(playerSelection, computerSelection));
+}
