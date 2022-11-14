ce756d0406868d5317c16e19336f472df97c6d69
Debbie <debby.ek@yahoo.com>
2022-11-14 15:01:26 -0600

    randomized a simple game of rock paper, scissors

--- rps.js
+++ rps.js
@@ -5,14 +5,14 @@ return choice[Math.floor(Math.random() * choice.length)];
 }
 
 let playerSelection = prompt('make your choice?');
-playerSelection.toUpperCase === playerSelection.toLowerCase;
+playerSelection = playerSelection;
 let computerSelection = getComputerChoice();
 function game1(playerSelection, computerSelection) {
-    if ((playerSelection == 'rock') && (computerSelection == 'paper')){
+    if (playerSelection == 'rock' && computerSelection == 'paper'){
         return 'You lose! paper beats rock!'
-    } else if ((playerSelection == 'scissors') && (computerSelection == 'rock')) {
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         return 'You lose! rock beats scissors!'
-    } else if ((playerSelection == 'paper') && (computerSelection == 'scissors')) {
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
        return 'You lose! scissors beats paper!'
     } else if (playerSelection == computerSelection){
         return 'A tie! Try again!'
