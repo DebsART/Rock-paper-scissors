1ef42abf46fe2e4525c352884aa47c58d44ec8ef
Debbie <debby.ek@yahoo.com>
2022-11-18 14:44:16 -0600

    looping game round

--- rps.js
+++ rps.js
@@ -7,17 +7,38 @@ return choice[Math.floor(Math.random() * choice.length)];
 let playerSelection = prompt('make your choice?');
 playerSelection = playerSelection.toLowerCase();
 let computerSelection = getComputerChoice();
-function game1(playerSelection, computerSelection) {
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'paper'){
-        return 'You lose! paper beats rock!'
+        return 'You lose! paper beats rock!' + ' Computer scores ' + ++computerScore + '!' + ' Your score in this round is ' + playerScore
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return 'You lose! rock beats scissors!'
+        return 'You lose! rock beats scissors!' + ' Computer scores ' + ++computerScore  + '!' + ' Your score in this round is ' + playerScore
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-       return 'You lose! scissors beats paper!'
+       return 'You lose! scissors beats paper!' + ' Computer scores ' + ++computerScore + '!' + ' Your score in this round is ' + playerScore
     } else if (playerSelection == computerSelection){
-        return 'A tie! Try again!'
+        return 'A tie! Try again! No scores allocated'
     } else{
-        return 'You win! great choice!'
+        return 'You win! great choice! Your Score in this round is ' + ++playerScore
     }
-    console.log(game1(playerSelection, computerSelection));
 }
+
+function game(playerScore, computerScore) {
+    for(let i=0; i<5; i++) {
+        playerSelection = prompt('make your choice?');
+        let gameRound = playRound(playerSelection, computerSelection);
+        return gameRound
+       
+        if (playerScore > computerScore) {
+            return 'You win! Game over!'
+        } if (computerScore > playerScore) {
+            return 'Computer win! Game over!'
+        } if (computerScore == playerScore) {
+           return 'Tie! Game over!'
+        }
+    }
+}
+
+        
+       
