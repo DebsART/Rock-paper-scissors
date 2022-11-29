a841ad351b4ae43822cfc521e697879c4c8ad33f
Debbie <debby.ek@yahoo.com>
2022-11-29 14:39:35 -0600

    adding event listener to buttons

--- rps.js
+++ rps.js
@@ -4,11 +4,29 @@ let choice = ['rock', 'paper', 'scissors'];
 return choice[Math.floor(Math.random() * choice.length)];
 }
 
-let playerSelection = prompt('make your choice?');
-playerSelection = playerSelection.toLowerCase();
 let computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
+
+let rock = document.querySelector("button.rock");
+let paper = document.querySelector("button.paper");
+let scissors = document.querySelector("button.scissors");
+
+let playerSelection = () => {
+rock.addEventListener("click", (function(){
+    console.log('rock');
+    playRound();
+}));
+paper.addEventListener("click", (function(){
+    console.log('paper');
+    playRound();
+
+}));
+scissors.addEventListener("click", (function(){
+    console.log('scissors');
+    playRound();
+}));
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'paper'){
@@ -23,6 +41,7 @@ function playRound(playerSelection, computerSelection) {
         return 'You win! great choice! Your Score in this round is ' + ++playerScore
     }
 }
+
 
 function game(playerScore, computerScore) {
     for(let i=0; i<5; i++) {
